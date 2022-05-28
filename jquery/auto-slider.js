03bf848e1638d107accba70071f740e7c6ac79f5
CDarkJames23 <adam.lnenicka2@gmail.com>
2022-05-28 09:15:16 +0100

    js stopped working

---
+++ jquery/auto-slider.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function ($) {
+  $(".auto-slider").slick({
+    speed: 9000,
+    autoplay: true,
+    autoplaySpeed: 0,
+    cssEase: "linear",
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    arrows: false,
+  });
+});
---
+++ jquery/slider.js
@@ -0,0 +1,36 @@
+$(".slider").slick({
+  dots: false,
+  ltl: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  prevArrow: $(".prev"),
+  nextArrow: $(".next"),
+  responsive: [
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
