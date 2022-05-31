ff22af8aa4760139fe212cca9129c090817aa3d4
CDarkJames23 <adam.lnenicka2@gmail.com>
2022-05-31 13:42:22 +0100

    commit all

---
+++ jquery/slider2.js
@@ -0,0 +1,37 @@
+$(".slider2").slick({
+  dots: false,
+  ltl: true,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 200,
+  speed: 8000,
+  variableWidth: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
