df577e021cf14d96b5f8be5f18d7cce11c20e81c
CDarkJames23 <adam.lnenicka2@gmail.com>
2022-06-02 01:00:13 +0100

    animations added

---
+++ jquery/slider2.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $(".Modern-Slider").slick({
+    autoplay: true,
+    autoplaySpeed: 2400,
+    speed: 1500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    pauseOnHover: false,
+    cssEase: "linear",
+    // fade:true,
+    draggable: false,
+  });
+});
